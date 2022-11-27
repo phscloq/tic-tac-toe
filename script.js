ab365cdba3e043b6cbb24428c975b151f5cba1ba
Baran Dogan <phscloq@gmail.com>
2022-11-27 13:33:06 +0300

    UPDATE script

--- script.js
+++ script.js
@@ -284,7 +284,24 @@ function randomMove(){
 
         
     }
+
+
+
+
+    
 }
+
+
+//AI will added later
+/* function isMovesLeft(board)
+{
+    for(let i = 0; i < 3; i++){
+        for(let j = 0; j < 3; j++)
+            if (board[i][j] != 'X' && board[i][j]!='O')
+                return true;
+    }
+    return false;
+} */
 
 
 return {checkWinner, beginGame, resetGame, gameStatus, randomMove};
